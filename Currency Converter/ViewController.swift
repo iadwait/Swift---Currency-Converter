4550bd94f0c8e0d40def6cab6465cabf1b250bdf
iAdwait <adwaitbarkale@gmail.com>
2020-09-08 10:48:48 +0530

    Data Fetched

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let url = URL(string: "http://data.fixer.io/api/latest?access_key=\(K.apiKey)") //Put Your API Key where i have written K.apiKey,You will get it from http://data.fixer.io
+    var myCurrency:[String] = []
+    var myValues:[Double] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getNetworkData(from: url!)
     }
 
+    func getNetworkData(from url:URL)
+    {
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil
+            {
+                print("Error Occured in Network Request")
+            }else{
+                if let data = data{
+                    do{
+                        let obj = try JSONDecoder().decode(CurrencyDataModel.self, from: data)
+                        for(key , value) in obj.rates
+                        {
+                            self.myCurrency.append(key)
+                            self.myValues.append(value)
+                        }
+                        print("MyCurrency = \(self.myCurrency)")
+                        print("MyValues = \(self.myValues)")
+                    }catch{
+                        print("Error Decoding Data")
+                    }
+                }
+                
+            }
+        }
+        task.resume()
+    }
 
 }
 
